41732366eccf008074dbc9056e3288b34e98ca51
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-08-31 07:03:53 +0530

    :heavy_plus_sign: Add locomotive-scroll framer-motion
    added locomotive-scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    transitionProperty: {
+      DEFAULT: '',
+    },
+    transitionDuration: {
+      DEFAULT: '300ms',
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
